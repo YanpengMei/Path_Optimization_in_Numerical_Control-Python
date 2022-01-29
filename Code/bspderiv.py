1d405455a290d3f990d8cec3774465172afff8e6
Yanpeng Mei <95927831+YanpengMei@users.noreply.github.com>
2022-01-29 22:52:07 +0100

    Add files via upload

---
+++ Code/bspderiv.py
@@ -0,0 +1,53 @@
+# Autor: Yanpeng Mei
+# Created at : 29.01.2022
+
+'''
+This function was originally written in Matlab by Mark Spink in 2000 and modified by Daniel Claxton in 2007.
+I re-write it in python.
+To avoid mistakes, the input and output(Type, Size and Value) of each function will be exactly the same as in Matlab !!!!!!
+'numpy' should be installed and imported !!!!!!
+'''
+
+'''
+bspderiv:  B-Spline derivative
+
+Meaning of the input:
+d - degree of the B-Spline
+c - control points            double  matrix(mc,nc)
+k - knot sequence           double  vector(nk)
+
+Meaning of the output:
+dc - control points of the derivative       double  matrix(mc,nc)
+dk - knot sequence of the derivative      double  vector(nk)
+
+Modification of Algorithm A3.3 from 'The NURBS BOOK' pg98
+'''
+
+import numpy as np
+
+
+def bspderiv(d, c, k):
+    [mc, nc] = np.shape(c)
+    nk = np.size(k)
+    dc = np.zeros((mc, nc - 1))
+    for i in range(nc - 1):
+        temp = d / (k[i+d+1] - k[i+1])
+        dc[:, i] = temp * (c[:, i+1] - c[:, i])
+    dk = k[1: (nk - 1)]
+    return dc, dk
+
+
+'''
+# test
+d = 3
+c = np.asarray([ [0, 10, 20, 30, 40, 50],
+                          [0, 5, -5, 5, -5, 0] ])
+k = np.asarray([0, 0, 0, 0, 1/3, 2/3, 1, 1, 1, 1])
+dc, dk = bspderiv(d, c, k)
+print('dc = ', dc)
+print('dk = ', dk)
+print('type of dk is ', type(dk))
+# dc should be [[ 90.  45.  30.  45.  90.]
+#                        [ 45. -45.  30. -45.  45.]]
+# dk should be [0.       0.       0.       0.33333333 0.66666667    1.      1.      1.]
+'''
---
+++ Code/bspeval.py
@@ -0,0 +1,54 @@
+# Autor: Yanpeng Mei
+# Created at : 29.01.2022
+
+'''
+This function was originally written in Matlab by Mark Spink in 2000 and modified by Daniel Claxton in 2007 and C. de Falco in 2010.
+I re-write it in python.
+To avoid mistakes, the input and output(Type, Size and Value) of each function will be exactly the same as in Matlab !!!!!!
+'numpy' should be installed and imported !!!!!!
+'''
+
+'''
+bspeval:  Evaluate B-Spline at parametric points
+
+Meaning of the input:
+d - Degree of the B-Spline.
+c - Control Points, matrix of size (dim,nc).
+k - Knot sequence, row vector of size nk.
+u - Parametric evaluation points, row vector of size nu.
+
+Meaning of the output:
+p - Evaluated points, matrix of size (dim,nu)
+'''
+
+import numpy as np
+from findspan import findspan
+from basisfun import basisfun
+
+
+def bspeval(d, c, k, u):
+    nu = np.size(u)
+    [mc, nc] = np.shape(c)
+    s = findspan(nc - 1, d, u, k)
+    b = basisfun(s, u, d, k)
+    temp1 = s - d + 1
+    p = np.zeros((mc, nu))
+    for i in range(d+1):
+        temp2 = np.tile(b[:, i].T, (mc, 1))
+        p = p +  temp2* c[:,temp1 + i - 1]
+    return p
+
+
+'''
+# test
+d = 3
+c = np.asarray([[0, 10, 20, 30, 40, 50],
+                         [0, 5,  -5, 5,  -5, 0 ]])
+k = np.asarray([0, 0, 0, 0, 1/3, 2/3, 1, 1, 1, 1])
+u = np.linspace(0,1,10)
+p = bspeval(d,c,k,u)
+print(p)
+# p should be
+#    [[ 0.   8.48765432   14.56790123    19.16666667    23.11728395    26.88271605   30.83333333    35.43209877    41.51234568    50.        ]
+#     [ 0.   2.19135802   0.86419753       -0.83333333     -0.58641975      0.58641975   0.83333333       -0.86419753    -2.19135802      0.        ]]
+'''
